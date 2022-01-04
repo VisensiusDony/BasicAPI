6bb8490a50dad6be717f9625b9b7b27919993e67
VisensiusDony <46786595+VisensiusDony@users.noreply.github.com>
2022-01-04 16:09:18 +0700

    CSS exercise

--- Client/Controllers/ExerciseController.cs
+++ Client/Controllers/ExerciseController.cs
@@ -14,7 +14,6 @@ namespace Client.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
